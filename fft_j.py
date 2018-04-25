1479393e2722c82d4571a3eeab6d7c1f010dc799
Clairvoyant95 <msubzda1@gmail.com>
2018-04-25 13:46:19 +0200

    PSA

--- fft_j.py
+++ fft_j.py
@@ -2,25 +2,19 @@ import wave
 import numpy
 import scipy
 
-my_file = wave.open("", rb) # otwieranie pliku
+my_file = wave.open('<sciezka>', 'rb') # otwieranie pliku
 
-instrument_list = ['organy':range(16, 15804),
-                   'fortepian':range(27, 4186),
-                   'kontrabas':range(41, 220),
-                   'skrzypce':range(196, 4186),
-                   'gitara':range(82,1047),
-                   'saksofon tenorowy':range(51,670),
-                   'flet':range(261,2093),
-                   'flet piccolo':range(587,4186)]
 
-# organy = (16 - 15804)
-# fortepian = (27 - 4186)
-# kontrabas = (41 - 220)
-# skrzypce = (196 - 4186)
-# gitara = (82 - 1047)
-# saksofon_tenorowy = (51 - 670)
-# flet = (261 - 2093)
-# flet_piccolo = (587 - 4186)
+organy = range(16, 15804)
+fortepian = range(27, 4186)
+kontrabas = range(41, 220)
+skrzypce = range(196, 4186)
+gitara = range(82, 1047)
+saksofon_tenorowy = range(51, 670)
+flet = range(261, 2093)
+flet_piccolo = range(587, 4186)
+
+instrument_list = [organy, fortepian, kontrabas, skrzypce, gitara, saksofon_tenorowy, flet, flet_piccolo]
 
 fregiencies_in_my_signal = numpy.fft.freq...(my_file)
 
